a5aedb625534f483e5a60ad9787ec5d95f365a59
wotj7687 <wotj7687@naver.com>
2019-11-18 16:44:42 +0900

    second version

---
+++ app/src/main/java/com/example/life9/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.life9
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //테스트 확인용입니다.
+}
